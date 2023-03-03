cc8ee9458c5b4b1984b00d34303a496378ba2227
Tanner Howell <a02079061@usu.edu>
2023-03-03 16:12:28 -0700

    More work on the wrapping issue

--- WritePoetry.java
+++ WritePoetry.java
@@ -1,11 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class WritePoetry {
     // IntelliJ suggested the FileNotFoundException to catch a bad file
-    public String writePoem(String file, String startWord, int length, boolean printHashtable) {
-
+    public String writePoem(String file, String startWord, int length, boolean printHashtable) {ArrayList<String> cars = new ArrayList<String>();
         if (printHashtable){
             // print the hashtable
         }
@@ -15,15 +15,20 @@ public class WritePoetry {
     public static void readAndBuild(String file)  throws FileNotFoundException{
         // Making the new hashtable
         HashTable<String, WordFreqInfo> table = new HashTable<>();
+        ArrayList<String[]> words = new ArrayList<String[]>();
 
         Scanner scanner = new Scanner(new File(file));
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            String[] words = line.split("[ ]");
+            String[] lineWords = line.split("[ ]");
+            words.add(lineWords);
+        }
 
+        for(int lineIndex = 0; lineIndex < words.size(); lineIndex++){
+            String [] currentLine = words.get(lineIndex);
             // Loop over the words in each line
-            for (int i = 0; i < words.length; i++) {
-                String word = words[i];
+            for (int i = 0; i < currentLine.length; i++) {
+                String word = currentLine[i];
 
                 // Check if the line is empty
                 if (!word.isEmpty()) {
@@ -34,14 +39,20 @@ public class WritePoetry {
                     if (lastChar == '.' || lastChar == ',' || lastChar == '!' || lastChar == '?') {
                         String wordWithoutPunc = word.substring(0, word.length() - 1);
                         table = addWordToTable(table, wordWithoutPunc, Character.toString(lastChar));
-                        if ( i < words.length - 1){
-                            table = addWordToTable(table, Character.toString(lastChar), words[i + 1]);
+                        if ( i < currentLine.length - 1){
+                            table = addWordToTable(table, Character.toString(lastChar), currentLine[i + 1]);
+                        } else{
+                            String followWord = words.get(lineIndex + 1)[0];
+                            table = addWordToTable(table, Character.toString(lastChar), followWord);
                         }
 
                     } else{
                         // Words without punctuation are just added to the table
-                        if ( i < words.length - 1){
-                            table = addWordToTable(table, word, words[i + 1]);
+                        if ( i < currentLine.length - 1){
+                            table = addWordToTable(table, word, currentLine[i + 1]);
+                        } else{
+                            String followWord = words.get(lineIndex + 1)[0];
+                            table = addWordToTable(table, word, followWord);
                         }
                     }
                 }
@@ -56,6 +67,16 @@ public class WritePoetry {
             WordFreqInfo WFI = new WordFreqInfo(wordToAdd, 0);
             table.insert(wordToAdd, WFI);
             table.find(wordToAdd).updateFollows(wordFollow);
+        }
+        return table;
+    }
+    private static HashTable<String, WordFreqInfo> addWordToTable (HashTable<String, WordFreqInfo> table, String wordToAdd){
+        if (table.contains(wordToAdd)){
+
+        } else{
+            WordFreqInfo WFI = new WordFreqInfo(wordToAdd, 1);
+            table.insert(wordToAdd, WFI);
+
         }
         return table;
     }
